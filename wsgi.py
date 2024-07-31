5f76fbb1ce9f5e19f5c0fd7f063cda3329f104a4
EC2 Default User <ec2-user@ip-10-0-1-220.ap-northeast-2.compute.internal>
2024-07-31 15:32:19 +0000

    서버 작동

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# DJANGO_SETTINGS_MODULE 환경 변수를 설정합니다.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# WSGI 애플리케이션을 생성합니다.
-application = get_wsgi_application()
